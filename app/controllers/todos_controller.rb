8f8460118c3e78b83b55bf31daffe1cd1132916f
mikey neilson <mikeyneilson35@googlemail.com>
2017-03-13 00:55:17 +0000

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,54 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+     flash[:notice] = "Todo was created sucssefully!"
+     redirect_to todo_path(@todo)
+    else
+      render "new"
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+   
+  end
+  def update
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was updated sucssefully"
+      redirect_to todos_path
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @todos = Todo.all
+  end
+
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo was deleted sucssefully"
+    redirect_to todos_path
+  end
+
+
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+end
